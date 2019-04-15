9ed804fa358e0ad525891ec9bf29e98989206380
Chris Campbell <chris@change.org>
2019-04-15 00:01:19 -0700

    Generates some stats

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const Octokit = require('@octokit/rest');
 const args = require('./lib/parse_args')(process);
+const processOrg = require('./lib/process_org');
+const mkTmpDir = require('./lib/mk_tmp_dir');
 
-const octokit = new Octokit();
+/* eslint-disable no-console */
+function dump(...msg) {
+  console.log(...msg);
+}
+function debug(...msg) {
+  console.log('[ghoot:debug]', ...msg);
+}
+function error(...msg) {
+  console.error('[ghoot:error]', ...msg);
+}
+/* eslint-enable no-console */
 
-const showOrg = async (org) => {
-  const data = await octokit.repos.listForOrg({ org });
-  console.log(data); // eslint-disable-line no-console
+const run = async () => {
+  processOrg({
+    tempDir: await mkTmpDir(),
+    ...args,
+    debug,
+    error,
+  })
+    .then(dump)
+    .catch(error);
 };
 
-showOrg(args._[0]);
+run();
